e83b8eb7820b9de67805906b948b863538480b6a
returndaniels <daniel.lipe@poli.ufrj.br>
2022-02-20 13:28:39 -0300

    Add mongooDB connection file

---
+++ database/index.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose');
+
+const databaseURL = process.env.MONGO_URI || 'mogodb://localhost/';
+
+console.log(databaseURL)
+
+mongoose.connect(databaseURL,
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+        useCreateIndex: true
+    });
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
