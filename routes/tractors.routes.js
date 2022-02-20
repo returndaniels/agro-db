40712a2df4d4e22c4b5f9be8c6f862859dcd25cf
returndaniels <daniel.lipe@poli.ufrj.br>
2022-02-20 14:28:27 -0300

    Changed route paths

--- routes/tractors.routes.js
+++ routes/tractors.routes.js
@@ -1,11 +1,11 @@
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const tractorControllers = require('../controllers/tractors.controllers');
 
-router.get("/", tractorControllers.getTractors);
+router.get('/', tractorControllers.getTractors);
 router.post('/', tractorControllers.createTractor);
-router.patch('/:name', tractorControllers.updateTractor);
-router.delete('/:name', tractorControllers.deleteTractor); 
+router.patch('/', tractorControllers.updateTractor);
+router.delete('/', tractorControllers.deleteTractor); 
 
 module.exports = router;
