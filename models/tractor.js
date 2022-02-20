67f6ece67ae62ec5f816ca0624187daaf357e9b3
returndaniels <daniel.lipe@poli.ufrj.br>
2022-02-20 13:29:16 -0300

    Add Tractor schema

---
+++ models/tractor.js
@@ -0,0 +1,30 @@
+const mongoose = require('../database');
+
+const tractorSchema = new mongoose.Schema(
+    {
+        name: {
+          type: String,
+          require: true,
+          unique : true,
+        },
+        model: {
+          type: String,
+          require: true
+        },
+        modelYear: {
+          type: Date
+        },
+        description: {
+          type: Boolean,
+        },
+        value: {
+          type: Number
+        },
+        imageUrl: {
+          type: String
+        }
+    }, { timestamps: true }
+);
+
+const Tractor = mongoose.model('Tractor', tractorSchema);
+module.exports = Tractor;
