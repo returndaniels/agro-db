ba025d2d6301f0883d8a3df98c7fffca58f0fdbc
returndaniels <daniel.lipe@poli.ufrj.br>
2022-02-20 13:58:00 -0300

    Add throw and dotenv config

--- database/index.js
+++ database/index.js
@@ -1,17 +1,19 @@
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const databaseURL = process.env.MONGO_URI || 'mogodb://localhost/';
 
 console.log(databaseURL)
 
 mongoose.connect(databaseURL,
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        useFindAndModify: true,
-        useCreateIndex: true
-    });
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}, err => {
+		if (err) throw err;
+		console.log('Connected to MongoDB!!!')
+	})
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
